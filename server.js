c6b236468cc2acd150b5c4c574ac85edca3b9237
GenioZx <eli.rodri14@email.com>
2025-07-21 18:03:45 -0400

    post and delete route added

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan')
 
 const app = express();
 
@@ -23,17 +25,39 @@ app.get("/", async (req, res) => {
 
 app.get('/cars', async (req, res) => {
     const allCars = await Car.find({});
-    res.render('cars-collections/index.ejs', {cars: allCars});
+    res.render('cars-collections/index.ejs', { cars: allCars });
 })
 
 app.get('/cars/new', (req, res) => {
     res.render('/cars-collections/new.ejs');
 });
 
-app.get('/cars/:carId', (req, res) => {
+app.get('/cars/:carId', async (req, res) => {
     const foundCar = await Car.findById(req.params.carId);
     res.render('cars-collections/show.ejs', { car: foundCar });
 });
+
+app.post('/cars', async (req, res) => {
+    if (req.body.isYourFavoriteCar === 'on') {
+        req.body.isYourFavoriteCar = true;
+    } else {
+        req.body.isYourFavoriteCar = false;
+    }
+    await Car.create(req.body);
+    res.redirect('/cars');
+});
+
+app.delete('/cars/:carId', async (req, res) => {
+    await Car.findByIdAndDelete(req.params.carId);
+    res.redirect('/cars');
+});
+
+app.get('/cars/:carId/edit', async (req, res) => {
+    const foundCar = await Fruit.findById(req.params.carId);
+    res.render('/cars-collections/edit.ejs', { car: foundCar });
+});
+
+
 
 app.listen(3000, () => {
     console.log('Listening to port 3000')
